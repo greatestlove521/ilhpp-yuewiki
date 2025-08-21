3044a86a357086c96a593a64bc0faedf8b5a7eb2
greatestlove521 <zhanghan1006@outlook.com>
2025-08-22 02:37:26 +0800

    Update intro.js

--- assets/intro.js
+++ assets/intro.js
@@ -6,7 +6,7 @@
  * |  Please discuss changes on the talk page, [[WP:VPT]] or GitHub before editing.  |
  * |_________________________________________________________________________________|
  *
- * Built from GitHub repository (https://github.com/diskdance/ilhpp), you should not make
+ * Built from GitHub repository (https://github.com/greatestlove521/ilhpp-yuewiki), you should not make
  * changes directly here.
  *
  * See https://github.com/diskdance/ilhpp/blob/main/README.MD#%E9%83%A8%E7%BD%B2%E6%96%B9%E6%B3%95 for build instructions.
