2af674f5dcfd56ce67369f73d0f127dd61fc4b9a
greatestlove521 <zhanghan1006@outlook.com>
2025-08-22 03:04:38 +0800

    Remove HanAssist due to yuewiki not need it

--- gadgets/ilhpp-settings/server/start.ts
+++ gadgets/ilhpp-settings/server/start.ts
@@ -1,11 +1,10 @@
 // Dev server entry point. This is not included in the build!
 import '../../../server/mockup_mw';
 import { createMwApp } from 'vue';
-import { batchConv } from 'ext.gadget.HanAssist';
 import messages from '../messages.json';
 import DevServer from './DevServer.vue';
 
-mw.messages.set(batchConv(messages));
+mw.messages.set(messages);
 
 // Bugged: https://github.com/typescript-eslint/typescript-eslint/issues/2865
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
